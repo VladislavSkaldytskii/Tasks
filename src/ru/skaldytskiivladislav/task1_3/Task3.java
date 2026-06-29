b2c0df7ca6877cea944304832a81bc0a37681d31
Vladislav Skaldytskiy <Svetovitd@yandex.ru>
2026-06-29 17:06:36 +0300

    completing task

--- src/ru/skaldytskiivladislav/task1_3/Task3.java
+++ src/ru/skaldytskiivladislav/task1_3/Task3.java
@@ -2,22 +2,15 @@ package ru.skaldytskiivladislav.task1_3;
 
 public class Task3 {
     public static void main(String[] args) {
+
         String name = "Владислав";
+        String str = "*";
+        String repeated = str.repeat(10);
 
         System.out.println("Меня зовут " + name);
-
         System.out.println(123 * 456);
 
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.print("*");
-        System.out.println("*");
+        System.out.println(repeated);
 
         System.out.print("Выводит");
         System.out.print(" несколько");
