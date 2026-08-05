cff677f9968a7046b5b9430ad572796c4a53c64e
Владислав <Vladislavskaldytskii@gmail.com>
2026-08-06 01:43:01 +0300

    correction task 2.2

--- src/ru/skaldytskiivladislav/task2_2/Calculator.java
+++ src/ru/skaldytskiivladislav/task2_2/Calculator.java
@@ -6,12 +6,12 @@ public class Calculator {
         int a = 17;
         int b = 5;
 
-        System.out.println("Cложение: " + "17 + 5 = " + (a + b));
+        System.out.println("Сложение: " + "17 + 5 = " + (a + b));
         System.out.println("Вычитание: " + "17 - 5 = " + (a - b));
-        System.out.println("Умножение: " + "17 + 5 = " + (a * b));
-        System.out.println("Целое деление: " + "17 + 5 = " + (a / b));
-        System.out.println("Остаток: " + "17 + 5 = " + (a % b));
-        System.out.println("Вещественное: " + "17 + 5 = " + ((double)a / b));
+        System.out.println("Умножение: " + "17 * 5 = " + (a * b));
+        System.out.println("Целое деление: " + "17 / 5 = " + (a / b));
+        System.out.println("Остаток: " + "17 % 5 = " + (a % b));
+        System.out.println("Вещественное деление: " + "17 / 5 = " + ((double) a / b));
 
     }
 }
