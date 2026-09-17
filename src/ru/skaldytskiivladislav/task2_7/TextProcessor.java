bdea082538cc40835eaa7b7d27d29e77dba59263
Владислав <Vladislavskaldytskii@gmail.com>
2026-09-17 16:35:42 +0300

    correction task 2.7

--- src/ru/skaldytskiivladislav/task2_7/TextProcessor.java
+++ src/ru/skaldytskiivladislav/task2_7/TextProcessor.java
@@ -11,31 +11,60 @@ public class TextProcessor {
         return words.length;
     }
 
-    public static String isValidEmail(String email) {
+    public static boolean isValidEmail(String email) {
         if (email == null || email.isEmpty()) {
-            return "Ошибка: Email не должен быть пустым: ";
+            //"Ошибка: Email не должен быть пустым: ");
+            return false;
         } else if (email.contains(" ")) {
-            return "Ошибка: пробелы запрещены";
+            return false; //"Ошибка: пробелы запрещены";
         } else if (email.indexOf("@") != email.lastIndexOf("@")) {
-            return "Ошибка: более одного символа @";
+            return false; //"Ошибка: более одного символа @";
+        } else if (email.contains("..")) {
+            return false; // Не должно быть 2х последовательных '.'
         }
 
         int atIndex = email.indexOf("@");
+        String localPart = email.substring(0, atIndex);
+        String domainPart = email.substring(atIndex + 1);
 
         if (atIndex <= 0) {
-            return "Ошибка: Символ @ не может быть первым символом.";
+            return false;//"Ошибка: Символ @ не может быть первым символом.";
         }
 
         int doIndex = email.indexOf(".", atIndex);
 
         if (doIndex == -1) {
-            return "Ошибка: после @ должна быть точка.";
+            return false; //"Ошибка: после @ должна быть точка.";
         } else if (doIndex == atIndex + 1) {
-            return "Ошибка: точка сразу после @. ";
+            return false;//Ошибка: точка сразу после @. ";
+        } else if (localPart.startsWith("-") || localPart.endsWith("-")) {
+            return false; // Не может начинаться и оканчиваться на '-'
         }
 
-        return "Верно";
+        for (char c : localPart.toCharArray()) {
+            if (!(Character.isLetterOrDigit(c) || c == '.' || c == '_' || c == '%' || c == '+' || c == '-')) {
+                return false; // локальная часть состоит только из букв, цифр, '.', '_', '%', '+', '-'
+            }
+        }
 
+        for (char c : domainPart.toCharArray()) {
+            if (!(Character.isLetterOrDigit(c) || c == '.' || c == '-')) {
+                return false; // доменная часть состоит только из букв, цифр, '.', '_'
+            }
+        }
+
+        String[] domainParts = domainPart.split("\\.");
+        String lastPart = domainParts[domainParts.length - 1];
+        if (lastPart.length() < 2) {
+            return false;
+        }
+
+        for (char c : lastPart.toCharArray()) {
+            if (!(Character.isLetter(c))) {
+                return false; //Корректный домен, т.е. должно заканчиваться на '.' и как минимум 2 буквы
+            }
+        }
+        return true;
     }
 
     public static boolean isPalindrome(String word) {
@@ -56,6 +85,12 @@ public class TextProcessor {
         System.out.println(isValidEmail("test@mail.com"));
         System.out.println(isValidEmail("test@@mail.com"));
         System.out.println(isValidEmail("test test@mail.com"));
+        System.out.println(isValidEmail("testtest@mail..com"));
+        System.out.println(isValidEmail("testtest@mail.c"));
+        System.out.println(isValidEmail("-testtest@mail.com"));
+        System.out.println(isValidEmail("-testt%est@mail.com"));
+        System.out.println(isValidEmail("te%st+@mail.co.uk"));
+
         System.out.println();
         System.out.println(isPalindrome("Hello"));
         System.out.println(isPalindrome("А роза упала на лапу Азора"));
